bf7ec9821002993df8c323518e24b87e7dff1ff6
Julio Torres <jtorres990@gmail.com>
2017-01-06 12:52:50 -0500

    friends

---
+++ src/Views/Friends/CardUser.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import CardUserCtrl from './main.ctrl';
+const moment = window.moment;
+
+export default class CardUser extends CardUserCtrl {
+    go = (e, obj) => {
+        e.preventDefault()
+        this.props.router.push(obj.href);
+    }
+    render = function() {
+        return (
+            <Card as="a" onClick={this.go} href={"/" + this.props.user.usuario + "/profile"}>
+                <Image src='/static/svg/user-3.svg' />
+                <Card.Content>
+                    <Card.Header>
+                        {this.props.user.nombres + " " + this.props.user.apellidos}
+                    </Card.Header>
+                    <Card.Meta>
+                        <span className='date'>
+                            Registrado en {moment(this.props.user.registrado).format("MMM Do YYYY")}
+                        </span>
+                    </Card.Meta>
+                </Card.Content>
+            </Card>
+        );
+    }
+}
--- src/Views/Friends/main.js
+++ src/Views/Friends/main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { Form } from 'semantic-ui-react';
+import { Form, Card } from 'semantic-ui-react'
 
+import CardUser from './CardUser';
 import FriendsCtrl from './main.ctrl';
 
 
@@ -10,8 +11,10 @@ export default class Friends extends FriendsCtrl {
         super(args);
         this.props.store.subscribe(this, ['friends'], "Friends");
     }
-    render = function() {
-        var str = JSON.stringify(this.props.store.getState().friends);
+    render = () => {
+        const friends = this.props.store.getState().friends || [];
+        var store = this.props.store;
+        var router = this.props.router;
         return (
             <div>
                 <Form onSubmit={this.onHandlerSearch}>
@@ -19,7 +22,12 @@ export default class Friends extends FriendsCtrl {
                         <Form.Input name="query"/>
                     </Form.Field>
                 </Form>
-                {str}
+                <br />
+                <Card.Group>
+                    {friends.map(function(value, index){
+                        return <CardUser key={index} store={store} router={router} user={value} />;
+                    })}
+                </Card.Group>
             </div>
         )
     }
