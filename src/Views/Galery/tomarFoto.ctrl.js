1d1854cf29ce01794c8ac3235e7b054b0542adbd
Julio Torres <jtorres990@gmail.com>
2017-02-01 12:46:32 -0500

    tomar foto

--- src/Views/Galery/tomarFoto.ctrl.js
+++ src/Views/Galery/tomarFoto.ctrl.js
@@ -1,18 +1,32 @@
 import React from 'react';
 const {Component} = React;
 
+
 export default class tomarFoto extends Component {
     handlerTomarFoto = () => {
-        /*var params = {audio: false, video: true};
-        var streamVideo = (streamVideo) => {
-            datosVideo.StreamVideo = streamVideo;
-            datosVideo.url = window.URL.createObjectURL(streamVideo);
-            $('#camara').attr('src', datosVideo.url);
-        };
-        var catchError = (error) => {
-            this.props.store.setState({error: "No se pudo acceder a la camara."})
-        }
-        navigator.getUserMedia(params, streamVideo, catchError);*/
-        alert("sd");
+        var captura = this.refs.webcam.getScreenshot();
+        this.captura = captura;
+        var canvas = this.refs.webcam.getCanvas();
+        canvas.toBlob((data) => {
+            this.capturaBlob = data;
+        });
+        this.forceUpdate();
+    }
+    handlerSubir = () => {
+        const
+            params = this.props.params || {},
+            galery = params.galery;
+        var data = new FormData();
+        data.append("name", "foto");
+        data.append("file", this.capturaBlob);
+        data.append("galery", galery);
+        this.props.store.galery.uploadFoto(data)
+            .then(() => this.props.router.push("/galery/" + galery));
+    }
+    handlerCancelar = () => {
+        const
+            params = this.props.params || {},
+            galery = params.galery;
+        this.props.router.push("/galery/" + galery);
     }
 }
--- src/Views/Galery/tomarFoto.js
+++ src/Views/Galery/tomarFoto.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-import { Button } from 'semantic-ui-react';
+import { Button, Image } from 'semantic-ui-react';
+import Webcam from 'react-webcam';
 
 import tomarFotoCtrl from './tomarFoto.ctrl';
 
+
 export default class tomarFoto extends tomarFotoCtrl {
+    componentDidMount() {
+    }
     render = () => {
+        if (this.captura) {
+            return (
+                <div>
+                    <Button primary onClick={this.handlerSubir}>Subir</Button>
+                    <Button primary onClick={this.handlerCancelar}>Cancelar</Button>
+                    <br />
+                    <br />
+                    <div><Image src={this.captura} /></div>
+                </div>
+            )            
+        }
         return (
             <div>
-                <Button onClick={this.handlerTomarFoto}>Tomar</Button>
-                <video ref={(el) => this.video = el} />
+                <Button primary onClick={this.handlerTomarFoto}>Tomar</Button>
+                <Button primary onClick={this.handlerCancelar}>Cancelar</Button>
+                <br />
+                <br />
+                <div><Webcam ref="webcam" audio={false} /></div>
             </div>
         )
     }
